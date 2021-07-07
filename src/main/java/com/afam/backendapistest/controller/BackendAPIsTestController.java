574eec255e83e97219409c89f44f926a26f30184
Afam <happyguy_afam@yahoo.com>
2021-07-07 10:32:48 +0100

    Added the pom file

--- src/main/java/com/afam/backendapistest/controller/BackendAPIsTestController.java
+++ src/main/java/com/afam/backendapistest/controller/BackendAPIsTestController.java
@@ -1,2 +1,13 @@
-package com.afam.backendapistest.controller;public class BackendAPIsTestController {
+package com.afam.backendapistest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BackendAPIsTestController {
+
+    @RequestMapping({"/apitest"})
+    public String firstPage(){
+        return "Hello World!!!";
+    }
 }
--- src/main/java/com/afam/backendapistest/service/JwtUserDetailsService.java
+++ src/main/java/com/afam/backendapistest/service/JwtUserDetailsService.java
@@ -1,2 +1,23 @@
-package com.afam.backendapistest.service;public class JwtUserDetailsService {
+package com.afam.backendapistest.service;
+
+import java.util.ArrayList;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if ("afamsecretkey".equals(username)) {
+            return new User("afamsecretkey", "$2a$10$4oRwOssn0GfECfwBYYH3oO46SUjAAwLQCmEsTUe9r2PWItxAJNb8S",
+                    new ArrayList<>());
+        } else {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+    }
 }
