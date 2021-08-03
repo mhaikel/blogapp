071952e493e1f905a457aaf042f0f6d74587f7d7
Afam <happyguy_afam@yahoo.com>
2021-08-03 17:28:41 +0100

    created trending news api homepage

---
+++ src/main/java/com/afam/backendapistest/model/Source.java
@@ -0,0 +1,30 @@
+package com.afam.backendapistest.model;
+
+public class Source {
+    private String id;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
